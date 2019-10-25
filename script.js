d58e21b3ccc4d6bf474e72931ee8ab5e19c30c0d
Niclas Koch <n.koch@medicalvision.de>
2019-10-25 12:13:09 +0200

    Stable

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ $(document).ready(function () {
     var startTxtO = $(".textStartO");
 
     var scrollChild = $(".scrollChild");
-    var scrollChild2 = $(".scrollChild2");
+    var scrollChild1 = $(".scrollChild1");
+    var scrollChildElement = $(".scrollChildElement");
     var verticalScroll = document.getElementById("vertScroll");
 
     var stickyDiv = document.getElementById("stickyDiv");
@@ -32,7 +33,33 @@ $(document).ready(function () {
                 opacity: 0.8,
                 margin: "0px",
             }, "fast");
+
+            scrollChild1.stop().animate({
+                height: '250px',
+                opacity: 0.8,
+                margin: "0px",
+            }, "fast");
+
+
+            scrollChild.css('border-bottom', '2px solid white');
+            scrollChild1.css('border-bottom', '2px solid white');
             verticalScroll.classList.add("verticalScroll");
+
+            scrollChild1.hover(function () {
+                $(this).addClass("hover");
+                scrollChildElement.fadeIn(100);
+                scrollChildElement.show();
+            }, function () {
+                $(this).removeClass("hover");
+                scrollChildElement.fadeOut(100);
+                scrollChildElement.hide();
+            });
+
+            scrollChild.hover(function () {
+                $(this).addClass("hover");
+            }, function () {
+                $(this).removeClass("hover");
+            });
         }
         if (window.pageYOffset <= topOffset) {
             stickyDiv.classList.remove("stickyAdd");
@@ -41,6 +68,9 @@ $(document).ready(function () {
                 opacity: 1.0,
                 margin: "0px",
             }, "fast");
+
+            scrollChild1.css('border-bottom', '0');
+            scrollChild.css('border-bottom', '0');
             verticalScroll.classList.remove("verticalScroll");
         }
     }
@@ -49,12 +79,14 @@ $(document).ready(function () {
         "mousedown": function () {
             startTxtR.css('display', 'none');
             red.css('box-shadow', '10px 0px 7px -5px rgba(0,0,0,0.4)');
+
             red.animate({
                 opacity: 1.0,
                 height: '600px',
             });
 
             wrapR.css('display', 'block').delay(150);
+
             wrapR.animate({
                 opacity: 1.0,
                 fontSize: '66px',
@@ -64,6 +96,7 @@ $(document).ready(function () {
         },
         "mouseup": function () {
             startTxtR.delay(360).show(0);
+
             red.animate({
                 opacity: 0.5,
                 height: '950px',
@@ -82,14 +115,17 @@ $(document).ready(function () {
     blue.on({
         "mousedown": function () {
             blue.css('box-shadow', '0px 0px 7px 5px rgba(0,0,0,0.9)');
+
             blue.animate({
                 opacity: 1.0,
             });
             blue.animate({
                 opacity: 1.0,
             }, 120);
+
             blue.css("transform", "translateX(1000px)");
             blue.css("transition-duration", "1s");
+
             startTxtB.delay(500).hide(0);
 
             wrapB.css("font-size", "90px", "!important");
@@ -104,12 +140,14 @@ $(document).ready(function () {
     yellow.on({
         "mousedown": function () {
             yellow.css('box-shadow', '10px 10px 7px -5px rgba(0,0,0,1.0)');
+
             yellow.animate({
                 height: "300px",
                 opacity: "1.0",
                 top: "0",
                 padding: "60px",
             }, "slow");
+
             wrapY.delay(600).show(0);
         },
     });
@@ -123,7 +161,7 @@ $(document).ready(function () {
 
             startTxtO.animate({
                 height: "100px",
-                paddingTop: "20px",                
+                paddingTop: "20px",
                 top: "0",
             }, "slow");
 
